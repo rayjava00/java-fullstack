3fd0119ce9553f3bac5b3e7e11fd9e4b97b9298e
rayjava00 <cristopherthompson00@gmail.com>
2023-05-15 11:59:55 +0200

    06 concatenazione variabili

--- es_08/calcolaPerimetroArea.java
+++ es_08/calcolaPerimetroArea.java
@@ -1,26 +1,24 @@
 
 
-import java.util.Scanner; //libreria che serve per utilizzare i metodi di scanner
-
+import java.util.Scanner; 
 public class calcolaPerimetroArea 
 
 
 {
     public static void main(String[] args)
     {
-      Scanner input = new Scanner(System.in);//creo un'oggetto scanner chiamato input
+      Scanner input = new Scanner(System.in);
       System.out.print("Inserisci la lunghezza del lato: ");
-      int lato; //questo sarà il paratmetro che utilizzerò per i calcoli 
+      int lato; 
       lato = input.nextInt();
       int area;
       int perimetro;
-      //lato = 5;
       area = lato*lato;
       perimetro = lato*4;
-      System.out.println(lato);
-      System.out.println (area);
-      System.out.println (perimetro); 
-      input.close(); //la chiusura è opzionale 
+      System.out.println("Il lato inserito: "+lato);//concateno testo e variabili
+      System.out.println ("L'area è: "+area);
+      System.out.println ("Il perimetro è: "+perimetro); 
+      input.close(); 
     }    
 }   
 
