76bdeeb7c5e6a71ed748fbbe6d366cf0fe8c2ca2
rayjava00 <cristopherthompson00@gmail.com>
2023-05-15 15:51:32 +0200

    upload es_09

---
+++ es_09/printNumbers.java
@@ -0,0 +1,21 @@
+
+
+public class printNumbers
+{
+    public static void main(String[] args)
+    {  
+        for (int i = 1; i <= 10; i++)
+        //for identifica il tipo di ciclo che si verifica per un numero predeterminato di volte 
+        //quante volte? Quanto è il valore che assume i
+        //i inizialmente è 1
+        //con il punto e virgola posso assegnare un altro valore ad i in questo caso 10 che rappresenta il numero massimo
+        //
+         {
+            System.out.println(i);
+
+
+         }
+    }    
+}   
+
+/*note per i collaboratori */
---
+++ es_10/forEach.java
@@ -0,0 +1,14 @@
+
+public class forEach 
+{
+    public static void main(String[] args)
+    {
+        int[] numbers= {1, 2, 3, 4, 5, 6, 7};
+        for (int num : numbers) 
+        {
+            int dim = numbers.length; //la classe array ha un metodo che descrive la quantità 
+         System.out.println("numero"+ num+" di "+dim+ " elementi") ;
+        }    
+    }   
+}
+
