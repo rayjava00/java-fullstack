a8c529937ac95fba1f69770e49b6f5e8dd010f3d
rayjava00 <cristopherthompson00@gmail.com>
2023-05-15 15:56:29 +0200

    upload es_10

--- es_10/forEach.java
+++ es_10/forEach.java
@@ -6,8 +6,8 @@ public class forEach
         int[] numbers= {1, 2, 3, 4, 5, 6, 7};
         for (int num : numbers) 
         {
-            int dim = numbers.length; //la classe array ha un metodo che descrive la quantità 
-         System.out.println("numero"+ num+" di "+dim+ " elementi") ;
+            int dim = numbers.length; //la classe array ha un metodo che descrive la quantità massima elementi
+         System.out.println("numero"+ num+" di "+dim+ " elementi") ; //concateno gli elementi con la lunghezza
         }    
     }   
 }
