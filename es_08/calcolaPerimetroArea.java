3c2508f430b12619a420df0901a00bf18891d7bc
rayjava00 <cristopherthompson00@gmail.com>
2023-05-15 12:12:45 +0200

    07 condizione if else

--- es_08/calcolaPerimetroArea.java
+++ es_08/calcolaPerimetroArea.java
@@ -15,11 +15,20 @@ public class calcolaPerimetroArea
       int perimetro;
       area = lato*lato;
       perimetro = lato*4;
+      //aggiunto una condizione che verifica l'input 
+      if (lato > 40){ //se l'input è > 40
+        System.out.println("Hai inserito un numero troppo grande"); 
+      }
+      else{ // se l'input <= 40
       System.out.println("Il lato inserito: "+lato);//concateno testo e variabili
       System.out.println ("L'area è: "+area);
       System.out.println ("Il perimetro è: "+perimetro); 
+      }
       input.close(); 
-    }    
+      //lascio fuori dal if else la chiusura della classe
+    }   
+
+
 }   
 
 
