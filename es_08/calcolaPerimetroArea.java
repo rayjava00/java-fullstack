6159ff0795abac3aece6f69d9c5fea8fd47dace2
rayjava00 <cristopherthompson00@gmail.com>
2023-05-15 10:50:25 +0200

    02 Inserita variabile lato per calcoli

--- es_08/calcolaPerimetroArea.java
+++ es_08/calcolaPerimetroArea.java
@@ -8,7 +8,8 @@ public class calcolaPerimetroArea //ricordarsi di cambiare il nome della classe
 {
     public static void main(String[] args)
     {
-        
+      int lato; //questo sarà il paratmetro che utilizzerò per i calcoli 
+  
     }    
 }   
 
