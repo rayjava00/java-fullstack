d927261d31801c9cea437b7c79a97f9616a56319
rayjava00 <cristopherthompson00@gmail.com>
2023-05-17 10:52:25 +0200

    caricamento es_14 manipolazione liste

---
+++ es_14/PrintList2.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PrintList2
+ {
+    public static void main(String[] args)
+    {
+     List<String> fruits = new ArrayList<String>();
+     fruits.add("Mela"); //il metodo add della classe aggiunge un elemento 
+     fruits.add("Banana");
+     fruits.add("Arancia");
+     Scanner input = new Scanner(System.in);
+     System.out.println("Quale frutta vuoi eliminare?: ");
+     int daElim = input.nextInt();
+     fruits.remove(daElim -1);
+     
+     for (String fruit : fruits)
+     
+     {
+     System.out.println(fruit);
+     
+     }
+    
+    }
+ }
